696d0044f0c0f04aedfd069b2995d930f7b11cdb
DJahich <r.kassanov@djahich.ru>
2024-08-13 14:26:00 +0300

    mkdir

--- script.sh
+++ script.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 
+
+mkdir scripts
+
+# Check if the directory was created successfully
+if [ $? -eq 0 ]; then
+    echo "The directory was created successfully."
+else
+    echo "Failed to create the  directory."
+fi
+
